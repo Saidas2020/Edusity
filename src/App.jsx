81dff7b811602029ceee724f52eefddb9b3ed85c
Saidas2020 <morvekarsai@gmail.com>
2025-06-28 16:38:45 +0530

    hii

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
-import Hero from './components/hero/Hero'
+import Hero from './components/Hero/Hero'
 import Programs from './components/Programs/Programs'
 import Title from './components/Title/Title'
 import About from './components/About/About'
